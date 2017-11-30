66f9205926a63d5c6b2efd5a97bf401ca31f4dc9
André Formento <andreformento.sc@gmail.com>
2017-11-30 09:51:15 -0200

    Delete start-libreoffice.sh

--- start-libreoffice.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-groupmod -g $gid libreoffice
-usermod -u $uid -g $gid libreoffice
-
-if [ -d /home/libreoffice/.config/libreoffice ]; then
-  chown -R libreoffice:libreoffice /home/libreoffice/.config/libreoffice
-fi
-
-exec su -ls "/bin/bash" -c "mkdir -p /home/libreoffice/.local/share; libreoffice5.3 " libreoffice
